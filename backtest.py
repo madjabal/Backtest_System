7e1d2eb8dea9e8adc8b55aaff187fb943e3c310c
madjabal <madjabal@bu.edu>
2020-07-10 15:38:14 -0400

    New file for terminal GUI

---
+++ backtest.py
@@ -0,0 +1,67 @@
+# Author: Michael Djaballah
+# Last edited  PM July 9, 2020
+# Last edited by: Michael Djaballah
+
+import yfinance as yf
+import pandas as pd
+from datetime import datetime
+import os
+from time import sleep, time
+from dateutil.relativedelta import relativedelta
+import sklearn as sk
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+# Takes no input
+# Output is newly saved CSV's containing the current makeup of the S&P 500 
+# and its historical additions and removals
+# data_path is changeable depending on desired save location
+def get_snp_store(data_path='data/'):
+    curr_raw = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+    
+    curr = curr_raw[0]
+    hist = curr_raw[1]
+    
+    new_hist = pd.DataFrame(hist['Date'])
+    new_hist['Added'] = hist['Added', 'Ticker']
+    new_hist['Removed'] = hist['Removed', 'Ticker']
+    
+    os.makedirs(data_path, exist_ok=True)
+    
+    curr.to_csv(data_path + 'snp_current.csv', index=False)
+    new_hist.to_csv(data_path + 'snp_hist.csv', index=False)
+    return None
+
+
+# Input: a date in string form with its corresponding format:
+# Ex: 'January 1, 2020', '%B %d, %Y'
+# Output: a list containing the S&P 500 at the input date
+def build_snp(date, date_format='%Y-%m-%d', data_path='data/'):
+#     get_snp_store(data_path=data_path)
+    
+    date_format2 = '%B %d, %Y'
+    
+    curr = pd.read_csv(data_path + 'snp_current.csv')
+    hist = pd.read_csv(data_path + 'snp_hist.csv')
+    
+    start_date = datetime.strptime(date, date_format)
+    
+    snp_set = set(curr['Symbol'])
+    
+    for i in range(len(hist)):
+        temp_date = datetime.strptime(hist.iloc[i]['Date'], date_format2)
+        if temp_date < start_date:
+            break
+
+        tb_removed = hist.iloc[i]['Added']
+        tb_added = hist.iloc[i]['Removed']
+
+        if tb_removed in snp_set:
+            snp_set.remove(tb_removed)
+        if not type(tb_added) == float:
+            snp_set.add(tb_added)
+    
+    return list(snp_set)
+
